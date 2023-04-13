586378ba8194d97a2ffa3cf68c9fa026f4f92fcc
ibrahim <ibrahimmuhieddine02@gmail.com>
2023-04-13 11:26:13 -0700

    added some questions to my game

---
+++ assets/scrip.js
@@ -0,0 +1,33 @@
+// quiz questions and answers
+const questions = [
+    {
+      question: "Inside which HTML element do we put the JavaScript",
+      options: [
+        "<js>;",
+        "var = blue;",
+        "<javascript>;",
+        "<script>;"
+      ],
+      answer: "<script>;"
+    },
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        options: [
+          "<head> and <body>",
+          "<head>",
+          "<body>",
+          "<footer>"
+        ],
+        answer: "<head> and <body>"
+      },
+      {
+        question: "How can you add a comment in a JavaScript?",
+        options: [
+          "//",
+          "<!-- blah -->",
+          "' '",
+        ],
+        answer: "//"
+      }
+    ];
+    
